8e41f529247dafba4e7063761ecd6384c29372e0
mahdi <mohammadmahdi.sokhanvar@gmail.com>
2025-05-13 18:09:02 +0330

    feat: add UrlRepository interface

---
+++ src/main/java/com/ziplink/repository/UrlRepository.java
@@ -0,0 +1,11 @@
+package com.ziplink.repository;
+
+import com.ziplink.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, UUID> {
+}
