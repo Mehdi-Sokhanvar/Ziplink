a969cc9432dbecfa776283d9466bf5c1b2de070d
Mehdi <mohammadmahdi.sokhanvar@gmail.com>
2025-05-14 12:06:29 +0330

    feat(config): add google.api.key for safe url

--- src/main/java/com/ziplink/dto/RegisterRequestDTO.java
+++ src/main/java/com/ziplink/dto/RegisterRequestDTO.java
@@ -1,13 +1,19 @@
 package com.ziplink.dto;
 
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public record RegisterRequestDTO
         (
                 @NotNull
-
+                @Email
+                @NotBlank
+                @NotEmpty
                 String email,
+                @NotNull
+                @Min(8)
+                @NotBlank
+                @NotEmpty
                 String password
         ) {
 }
